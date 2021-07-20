a83f0fba7fa597593703be2761a214a70f25d9ea
mahesh <kmaheshshetty1@gmail.com>
2021-07-20 13:01:33 +0530

    added product

--- src/main/java/product.java
+++ src/main/java/product.java
@@ -3,6 +3,8 @@ public class product {
 	
 	public static void main(String[] args) {
 		System.out.println("HELLO Product");
+		System.out.println("new Product");
+		System.out.println("App");
 	}
 
 }
